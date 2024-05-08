29d095099265978ce47d6c163a063f72f3f2208e
Karyna Avramenko <karinmia99avr@gmail.com>
2024-05-09 01:38:55 +0300

    Send and receive messages via websockets; css fixes

--- app/routers/api.py
+++ app/routers/api.py
@@ -1,17 +1,17 @@
 import time
 from typing import Annotated, Union
 
-from fastapi import APIRouter, Depends, Request, WebSocket, Body, Response, status
+from fastapi import APIRouter, Depends, Request, WebSocket, WebSocketDisconnect, Response, status
 from fastapi.responses import JSONResponse, HTMLResponse, RedirectResponse
-from fastapi.templating import Jinja2Templates
-from pydantic import BaseModel
 
+from app.database import get_channels_list
 from app.dependencies import verify_session_token
-from app.utils import generate_session_token, generate_random_user_name
+from app.schemas import Channel
+from app.utils import generate_random_user_name
+from app.websocket import ConnectionManager
 
 api_router = APIRouter(prefix="/api")
 
-# templates = Jinja2Templates(directory="templates")
 
 @api_router.get("/db")
 async def healthcheck(request: Request):
@@ -24,20 +24,11 @@ async def healthcheck(request: Request):
 
 
 @api_router.get("/channels", dependencies=[Depends(verify_session_token)])
-async def channels(channel_id: str):
-    print("api :: channels")
-    # context['channels'] = await get_channels_list(request.app.db)
-    return {
-        "status": "success",
-        # "channels": session
-    }
+async def channels(request: Request) -> list[Channel]:
+    return await get_channels_list(request.app.db)
 
 @api_router.get("/channels/{channel_id}/messages", dependencies=[Depends(verify_session_token)])
 async def messages(channel_id: str, request: Request):
-    print("api :: get messages")
-    print(f"{channel_id = }")
-    # context['channels'] = await get_channels_list(request.app.db)
-
     messages = [
         {
             "channel_id": channel_id,
@@ -61,25 +52,18 @@ async def messages(channel_id: str, request: Request):
     return request.app.templates.TemplateResponse(
         request=request, name="messages.html", context={'messages': messages}
     )
-    # return {
-    #     "status": "success",
-    #     "messages": messages
-    # }
 
+manager = ConnectionManager()
 
-# @api_router.get("/items/{item_id}")
-# def read_item(item_id: int, q: Union[str, None] = None):
-#     return {"item_id": item_id, "q": q}
-
-
-# @api_router.put("/items/{item_id}")
-# def update_item(item_id: int, item: Item):
-#     return {"item_name": item.name, "item_id": item_id}
-
-
-# @api_router.websocket("/ws")
-# async def websocket_endpoint(websocket: WebSocket):
-#     await websocket.accept()
-#     while True:
-#         data = await websocket.receive_text()
-#         await websocket.send_text(f"Message text was: {data}")
+@api_router.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_json()
+            print(f"--- received message from websocket: {data}")
+            # await manager.send_personal_message(data, websocket)
+            await manager.broadcast_json(data)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        # await manager.broadcast(f"Client ??? left the chat")
